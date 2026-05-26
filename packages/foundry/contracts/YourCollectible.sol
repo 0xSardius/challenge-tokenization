ce24dbbc877ebfbd8c1ffca17313c3a510093b83
Sardius <0xSardius@gmail.com>
2026-05-26 09:56:19 -0400

    checkpoint 1: add ERC-721 imports and inheritance

--- packages/foundry/contracts/YourCollectible.sol
+++ packages/foundry/contracts/YourCollectible.sol
@@ -6,47 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    uint256 public tokenIdCounter;
+contract YourCollectible
+    is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable
+{
+    // TODO[state]: State variables, constructor, and base URI will be added here
+    // Complete the "Token State & Constructor" checkpoint to unlock
 
-    constructor() ERC721("YourCollectible", "YCB") Ownable(msg.sender) { }
+    // TODO[mint]: The minting function will be added here
+    // Complete the "Minting NFTs" checkpoint to unlock
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs/";
-    }
-
-    function mintItem(address to, string memory uri) public returns (uint256) {
-        tokenIdCounter++;
-        uint256 tokenId = tokenIdCounter;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        return tokenId;
-    }
-
-    // Override functions from OpenZeppelin ERC721, ERC721Enumerable and ERC721URIStorage
-
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
-    }
-
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+    // TODO[overrides]: Override functions will be added here
+    // Complete the "Inheritance & Overrides" checkpoint to unlock
 }
